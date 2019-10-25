e25a1bea44fe9ca6cec98cea0b0ac206039b60af
den <dennis.d.langer@ntnu.no>
2019-10-25 19:17:34 +0200

    Apped init script and supply ship image file

---
+++ simulink/init.m
@@ -0,0 +1,22 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% init()                                                                  %
+%                                                                         %              
+% Set initial parameters for part1.slx and part2.slx                      %
+%                                                                         %
+% Created:      2018.07.12	Jon Bj�rn�                                    %
+%                                                                         %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear all;
+
+load('supply.mat');
+load('supplyABC.mat');
+load('thrusters_sup.mat');
+
+% Initial position x, y, z, phi, theta, psi
+eta0 = [0,0,0,0,0,0]';
+% Initial velocity u, v, w, p, q, r
+nu0 = [0,0,0,0,0,0]';
+%%
+SimulationToRun = 3;
+useRefM = 1;
