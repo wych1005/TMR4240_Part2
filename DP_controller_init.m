7b39b2d02cb2d1f700fcded1bd74356abbc5ac9b
somlioy <somlioy@gmail.com>
2019-10-28 11:49:49 +0100

    Added init files for DP-controller and reference model

---
+++ DP_controller_init.m
@@ -0,0 +1,7 @@
+% PID-Controller gains
+Kp = [6e5 4e5 2e8];
+Ki = [3e4 1e4 9e5];
+Kd = [3e6 3e6 6e8];
+
+% Integrator saturation
+int_sat = [8e5 8e5 2e6];
---
+++ reference_model_init.m
@@ -0,0 +1,10 @@
+% Use Refecende model?
+useRefM = 1;
+
+% Reference model
+omega_refm = [0.03 0.03 0.03];
+delta_refm = [1 1 1];
+
+% Saturation limits
+nu_sat = [inf inf inf]; % Velocity
+nu_dot_sat = [inf inf inf]; % Acceleration
