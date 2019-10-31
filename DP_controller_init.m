958462fc20eef1f869faedcd28f2e97032036f02
den <dennis.d.langer@ntnu.no>
2019-10-31 17:58:33 +0100

    Integrated thrust allocation into the thrust allocation simulink diagram

--- DP_controller_init.m
+++ DP_controller_init.m
@@ -1,7 +1,7 @@
 % PID-Controller gains
-Kp = [6e5 4e5 2e8];
-Ki = [3e4 1e4 9e5];
-Kd = [3e6 3e6 6e8];
+Kp = [2e3 4e4 5e4];
+Ki = [1e1 1e2 1e4];
+Kd = [1e3 1e5 5e4];
 
 % Integrator saturation
 int_sat = [8e5 8e5 2e6];
