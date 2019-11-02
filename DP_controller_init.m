4d6cef9ddef4c94f7f40461e0d86d382a3976ded
somlioy <somlioy@gmail.com>
2019-11-02 13:26:00 +0100

    PID-tuning

--- DP_controller_init.m
+++ DP_controller_init.m
@@ -1,6 +1,6 @@
-% PID-Controller gains
-Kp = [2e4 4e4 3e7];
-Ki = [8e1 8e1 1e4];
+    % PID-Controller gains
+Kp = [2e5 8e4 3e7];
+Ki = [8e2 4e2 1e4];
 Kd = [4e4 1e4 4e3];
 
 % Integrator saturation
