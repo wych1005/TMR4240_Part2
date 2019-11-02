9d9f0cec92231bb014175296e254c03d0951ca19
somlioy <somlioy@gmail.com>
2019-11-02 22:43:04 +0100

    Tuning

--- DP_controller_init.m
+++ DP_controller_init.m
@@ -1,7 +1,11 @@
     % PID-Controller gains
-Kp = [2e5 8e4 3e7];
-Ki = [8e2 4e2 1e4];
-Kd = [4e4 1e4 4e3];
+Kp = [7e5 8e4 3e7];
+Ki = [8e3 1e3 1e5];
+Kd = [4e6 1e4 4e3];
 
 % Integrator saturation
 int_sat = [8e5 8e5 2e5];
+
+% Kp = [6e5 4e4 2e8];
+% Ki = [3e3 1e2 9e5];
+% Kd = [3e6 3e4 6e8];
